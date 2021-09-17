04300ca3156a5ffddeabc5dcb76890091ecfdfb3
Johannes Maron <johannes@maron.family>
2021-09-17 15:34:46 +0200

    Update packaging and CI suite

--- tests/testapp/models.py
+++ tests/testapp/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 from dynamic_filenames import FilePattern
 
-
 upload_to_pattern = FilePattern(
-    filename_pattern='{instance.title:slug}{ext}',
+    filename_pattern="{instance.title:slug}{ext}",
 )
 
 
 class DefaultModel(models.Model):
-    title = models.CharField(max_length=100, default='hello goodby')
+    title = models.CharField(max_length=100, default="hello goodby")
     file_field = models.FileField(upload_to=upload_to_pattern)
